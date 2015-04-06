7522466648a596cb66f53802e02e0f1c2fad483f
johannes karoff <johannes@karoff.net>
2015-04-06 22:48:24 +0200

    add yolk for interactively moving files

---
+++ yolk/interactive-mv.zsh
@@ -0,0 +1,12 @@
+# interactively move/rename files
+# example: interactive-mv src/*.c
+
+interactive-mv() {
+  local src dst
+  for src; do
+    [[ -e $src ]] || { print -u2 "$src does not exist"; continue }
+    dst=$src
+    vared dst
+    [[ $src != $dst ]] && mkdir -p $dst:h && mv -n $src $dst
+  done
+}
