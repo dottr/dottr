90f7bd601bb9de3e84d378638a7911f64ea22c5b
johannes karoff <johannes@karoff.net>
2015-04-07 13:36:27 +0200

    fix dottr.plugin.zsh file

--- dottr.plugin.zsh
+++ dottr.plugin.zsh
@@ -1,1 +1,5 @@
-pan.zsh
+export DOTTRHOME=$(dirname "$(/bin/readlink -f "$0")")
+
+fry () {
+    source $DOTTRHOME/yolk/"$@".zsh
+}
--- pan.zsh
+++
@@ -1,5 +0,0 @@
-export DOTTRHOME=$(dirname "$(/bin/readlink -f "$0")")
-
-fry () {
-    source $DOTTRHOME/yolk/"$@".zsh
-}
