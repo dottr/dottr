b97db71c819e9705f6c085d9ac2dfee0bc38d43e
johannes karoff <johannes@karoff.net>
2015-04-10 13:19:49 +0200

    make smart-cd overwrite cd

--- yolk/smart-cd.zsh
+++ yolk/smart-cd.zsh
@@ -1,7 +1,7 @@
 # smart cd function, e.g., switch to /etc when issuing /etc/hosts
 # example: smart-cd /etc/hosts
 
-smart-cd() {
+cd() {
     if (( ${#argv} == 1 )) && [[ -f ${1} ]]; then
         [[ ! -e ${1:h} ]] && return 1
         print "Correcting ${1} to ${1:h}"
