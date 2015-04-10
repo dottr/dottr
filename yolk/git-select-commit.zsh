1e0926a7d7370c1605a68e111eb69571413221c2
Felix Dietze <felix@beerleader.net>
2015-04-10 22:31:24 +0200

    don't interpret escape sequences

--- yolk/git-select-commit.zsh
+++ yolk/git-select-commit.zsh
@@ -3,5 +3,5 @@
 
 git-select-commit() {
     gitlog=$(git log --pretty=format:'%Cred%h %C(reset)%C(dim)%ad%x08%x08%x08%x08%x08%x08%x08%x08%x08%Creset %s%C(yellow)%d%C(reset) %C(blue)<%an> %Cgreen(%ar)%Creset' --abbrev-commit --date=iso --date-order)
-    echo $gitlog | fzf --no-sort --ansi | cut -f1 | while read -r hash description; do $@ "$hash"; done
+    echo -E $gitlog | fzf --no-sort --ansi | cut -f1 | while read -r hash description; do $@ "$hash"; done
 }
