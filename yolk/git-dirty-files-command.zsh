7ab5deb02159b446c29475482bf3f0d5edffedcb
johannes karoff <johannes@karoff.net>
2015-04-10 14:03:10 +0200

    fix git-dirty-files-command if pwd is not git root

--- yolk/git-dirty-files-command.zsh
+++ yolk/git-dirty-files-command.zsh
@@ -2,7 +2,8 @@
 # example: git-dirty-files-command git add
 
 git-dirty-files-command() {
+    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
     files=$(git status --porcelain | awk '{for (i=2; i<=NF; i++) print $i}' | fzf --multi)
 
-    eval "$@ $(echo $files | xargs -I'{}' echo -n '"{}" ')"
+    eval "$@ $(echo "$files" | xargs -I'{}' echo -n '"$git_root/{}" ')"
 }
