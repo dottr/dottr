abf2cc42f5ec9e45606863a7ce45d21a6331c84b
johannes karoff <johannes@karoff.net>
2015-04-10 19:03:27 +0200

    do not set formatter variable in neo4j-query yolk

--- yolk/neo4j-query.zsh
+++ yolk/neo4j-query.zsh
@@ -5,14 +5,12 @@
 # NEO4J_QUERY_JSON_FORMATTER="underscore print --color --outfmt json"
 # NEO4J_QUERY_JSON_FORMATTER="json_pp"
 
-NEO4J_QUERY_JSON_FORMATTER="cat"
-
 neo4j-query() {
     JSON=$(curl --silent -H "Content-Type: application/json" \
         -d "{\"statements\":[{\"statement\":\"$*\",\"resultDataContents\":[\"graph\",\"row\"]}]}" \
         http://localhost:7474/db/data/transaction/commit)
 
-    eval "echo '$JSON' | $NEO4J_QUERY_JSON_FORMATTER"
+    eval "echo '$JSON' | ${NEO4J_QUERY_JSON_FORMATTER:-cat}"
 }
 
 neo4j-clear() {
