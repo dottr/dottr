515b8469385ea13f96fd2a6bc8c56ec7078ca863
johannes karoff <johannes@karoff.net>
2015-07-18 01:38:39 +0200

    no --ansi option for fzf in search-select-edit

--- yolk/zsh/search-select-edit.zsh
+++ yolk/zsh/search-select-edit.zsh
@@ -4,7 +4,7 @@
 
 search-select-edit () {
     results=$(ag --filename --noheading --numbers --column --nobreak --color -S $@) 
-    selected=$(echo -E $results | fzf --ansi --multi --no-sort --tac) 
+    selected=$(echo -E $results | fzf --multi --no-sort --tac) 
     [[ -z "$selected" ]] && return 0
     uniquefileswithlines=$(echo -E $selected | tac | sed '$!N; /^\([^:]*\):.*\n\1:.*$/!P; D' | tac | cut -d: -f1-3) 
     eval "vim $(echo -E $uniquefileswithlines | xargs -I'{}' echo -E -n '"{}" ')"
