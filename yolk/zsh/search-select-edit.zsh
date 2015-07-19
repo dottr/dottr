5b90023754444ede02c804b69558a465ec4fd861
Felix Dietze <felix@beerleader.net>
2015-07-19 15:54:16 +0200

    hidden file support

--- yolk/zsh/search-select-edit.zsh
+++ yolk/zsh/search-select-edit.zsh
@@ -3,7 +3,7 @@
 # TODO: use custom replacements for ag / vim / fzf
 
 search-select-edit () {
-    results=$(ag --filename --noheading --numbers --column --nobreak --color -S $@) 
+    results=$(ag --filename --noheading --numbers --column --nobreak --color --hidden -S $@) 
     selected=$(echo -E $results | fzf --ansi --multi --no-sort --tac) 
     [[ -z "$selected" ]] && return 0
     uniquefileswithlines=$(echo -E $selected | tac | sed '$!N; /^\([^:]*\):.*\n\1:.*$/!P; D' | tac | cut -d: -f1-3) 
