7ce4ef8892ad9b56e6d55a1de98dce91fa9da95f
Felix Dietze <felix@beerleader.net>
2016-02-06 20:53:56 +0100

    pass rest of parameters to git

--- yolk/zsh/github-clone.zsh
+++ yolk/zsh/github-clone.zsh
@@ -1,6 +1,9 @@
 # git clone repositories from github
 # example: github-clone dottr/dottr
+# example: github-clone rust-lang/rust --depth=1
 
 github-clone() {
-    git clone git@github.com:$@
+    REPO=$1
+    shift 1
+    git clone git@github.com:$REPO $@
 }
