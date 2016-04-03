f55a23207c4f929386e0ab8ec9495cda7501643e
Felix Dietze <felix@beerleader.net>
2016-04-04 00:43:51 +0200

    transcode video to gif

--- yolk/zsh/transcode-video.zsh
+++ yolk/zsh/transcode-video.zsh
@@ -8,3 +8,9 @@ transcode-video() {
     # default video quality: H264(23)
     ffmpeg -i "$1" -acodec libvorbis -aq 6 -vcodec libx264 -crf 23 -preset slow "$1.mkv"
 }
+
+transcode-video-gif() {
+    ffmpeg -i "$1" -filter:v palettegen "${1}_palette.png"
+    ffmpeg -i "$1" -i "${1}_palette.png" -filter_complex "paletteuse" "$1.gif"
+    rm "${1}_palette.png"
+}
