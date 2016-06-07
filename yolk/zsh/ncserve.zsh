46831c4a62bcef23534701bc833bf3816c996267
Felix Dietze <felix@beerleader.net>
2016-06-08 00:27:42 +0200

    ipfs implementation to send files

--- yolk/zsh/ncserve.zsh
+++ yolk/zsh/ncserve.zsh
@@ -40,3 +40,11 @@ ncrecv () {
 nclisten () {
      nc -v -q0 -l 1337 | tar -kxv
 }
+
+ipfsserve() {
+    pw=$(pwgen -s 20 1)
+    bytes=$(du -cb "$@" | tail -n1 | awk '{print $1}')
+    hash=$(tar -cvf - testdir | aespipe -e AES256 -p3 3< <(echo $pw) | pv -s $bytes | ipfs add -p -q)
+    echo "ipfs cat $hash | aespipe -e AES256 -p3 -d 3< <(echo $pw) | pv -s $bytes | tar -kxv"
+}
+
