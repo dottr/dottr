1345bbe99e77208ac5a26514cdde3bf398875446
johannes karoff <johannes@karoff.net>
2016-09-02 19:48:28 +0000

    provide auth header in neo4j-query commands
    - reads env vars NEO4J_USER, NEO4J_PASS
    - defaults to neo4j:neo4j

--- yolk/zsh/neo4j-query.zsh
+++ yolk/zsh/neo4j-query.zsh
@@ -4,9 +4,13 @@
 # replace the json formatter with your preferred one. For example:
 # NEO4J_QUERY_JSON_FORMATTER="underscore print --color --outfmt json"
 # NEO4J_QUERY_JSON_FORMATTER="json_pp"
+# NEO4J_USER
+# NEO4J_PASS
 
 neo4j-query() {
-    JSON=$(curl --silent -H "Content-Type: application/json" \
+    user="${NEO4J_USER:-neo4j}"
+    pass="${NEO4J_PASS:-neo4j}"
+    JSON=$(curl --user '$user:$pass' --silent -H "Content-Type: application/json" \
         -d "{\"statements\":[{\"statement\":\"$*\",\"resultDataContents\":[\"graph\",\"row\"]}]}" \
         http://localhost:7474/db/data/transaction/commit)
 
