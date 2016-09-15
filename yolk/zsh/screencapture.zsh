966aa78c6bf4298b1b2b53e8b3e3eebf3c698b59
Felix Dietze <felix@beerleader.net>
2016-09-15 17:30:56 +0200

    hq screencast

--- yolk/zsh/screencapture.zsh
+++ yolk/zsh/screencapture.zsh
@@ -1,10 +1,16 @@
 # record and encode screen and microphone to Vorbis/H264
 
-# dependency: ffmpeg with H264
+# dependency: ffmpeg with H264, alsa or pulseaudio-alsa, xdpyinfo
 # TODO: use free codec if H264 is not available
 
 screencapture() {
     # Audio: Mono, Vorbis(5)
     # Video: Display :0.0, Sreen Resolution, 25fps, H264, default quality:23
-    ffmpeg -y -f alsa -ac 1 -i pulse -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -i :0.0 -acodec libvorbis -aq 5 -vcodec libx264 -crf 23 -preset superfast -r 25 capture-$(date +"%Y-%m-%d-%H-%M-%S").mkv
+    ffmpeg -y -f alsa -ac 1 -i pulse -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -i $DISPLAY -acodec libvorbis -aq 5 -vcodec libx264 -crf 23 -preset superfast -r 25 capture-$(date +"%Y-%m-%d-%H-%M-%S").mkv
+}
+
+screencaptureHQ() {
+    # Audio: Mono, Vorbis(5)
+    # Video: Display :0.0, Sreen Resolution, 25fps, H264, default quality:18
+    ffmpeg -y -f alsa -ac 1 -i pulse -f x11grab -s `xdpyinfo | grep 'dimensions:'|awk '{print $2}'` -i $DISPLAY -acodec libvorbis -aq 5 -vcodec libx264 -crf 18 -preset superfast -r 30 capture-$(date +"%Y-%m-%d-%H-%M-%S").mkv
 }
