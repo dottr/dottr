400fbda924d0130eac45af262146ab02c6e1ac3d
Felix Dietze <felix@beerleader.net>
2017-02-28 22:10:37 +0100

    simpler git-select-commit, simpler log date format

--- yolk/zsh/git-select-commit.zsh
+++ yolk/zsh/git-select-commit.zsh
@@ -1,7 +1,7 @@
-# interactively select git commit from current branch and execute command
-# example: git-select-commit git checkout
+# interactively select git commit and write it to stdout
+# example: git checkout $(git-select-commit)
 
 git-select-commit() {
-    gitlog=$(git log --pretty=format:'%Cred%h %C(reset)%C(dim)%ad%x08%x08%x08%x08%x08%x08%x08%x08%x08%Creset %s%C(yellow)%d%C(reset) %C(blue)<%an> %Cgreen(%ar)%Creset' --abbrev-commit --date=iso --date-order)
-    echo -E $gitlog | fzf --no-sort --ansi | cut -f1 | while read -r hash description; do $@ "$hash"; done
+    gitlog=$(git log --pretty=format:'%Cred%h %C(reset)%C(dim)%ad%Creset %s%C(yellow)%d%C(reset) %C(blue)<%an> %Cgreen(%ar)%Creset' --abbrev-commit --date=iso --date-order --date="format:%F %R" $@)
+    echo -E $gitlog | fzf --no-sort --ansi | cut -d ' ' -f1
 }
