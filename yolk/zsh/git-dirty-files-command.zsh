21c249ea2976e381e96ee2e820a9baf82dab8161
Felix Dietze <felix@beerleader.net>
2017-04-08 21:25:15 +0200

    provide separate command to just list dirty files

--- yolk/zsh/git-dirty-files-command.zsh
+++ yolk/zsh/git-dirty-files-command.zsh
@@ -1,10 +1,16 @@
 # issue command on manually selected dirty git files
 # example: git-dirty-files-command git add
+# example: rm $(git-select-dirty-files)
+
+git-select-dirty-files() {
+    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
+    git status --porcelain | awk '{for (i=2; i<=NF; i++) print $i}' | fzf --multi
+}
 
 git-dirty-files-command() {
-    git_root=$(git rev-parse --show-toplevel 2> /dev/null)
-    files=$(git status --porcelain | awk '{for (i=2; i<=NF; i++) print $i}' | fzf --multi)
+    files=$(git-select-dirty-files)
     [[ -z "$files" ]] && return 0
 
     eval "$@ $(echo "$files" | xargs -I'{}' echo -n '"$git_root/{}" ')"
 }
+
